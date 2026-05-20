5624518d21953267fd491200049946894ada5bc1
Kallik <h.jarqueg@gmail.com>
2026-05-20 12:03:54 +0200

    [feature/create-models]: create workExperience model

--- src/main/kotlin/org/example/cvgenerator/domain/model/WorkExperience.kt
+++ src/main/kotlin/org/example/cvgenerator/domain/model/WorkExperience.kt
@@ -1,5 +1,12 @@
 package org.example.cvgenerator.domain.model
 
 data class WorkExperience(
-    val id: Int,
+    val company: String = "",
+    val position: String = "",
+    val location: String = "",
+    val startDate: String = "",
+    val endDate: String = "",
+    val isCurrent: Boolean = false,
+    val description: String = "",
+    val bulletPoints: List<String> = emptyList(),
 )
