fd88493e2d1c83af50ea194d9681a10399adb83b
Kallik <h.jarqueg@gmail.com>
2026-05-20 12:01:56 +0200

    [feature/create-models]: create CV model

--- src/main/kotlin/org/example/cvgenerator/domain/model/Cv.kt
+++ src/main/kotlin/org/example/cvgenerator/domain/model/Cv.kt
@@ -1,5 +1,10 @@
 package org.example.cvgenerator.domain.model
 
 data class Cv(
-    val id: Int,
+    val personalInfo: PersonalInfo = PersonalInfo(),
+    val workExperience: List<WorkExperience> = emptyList(),
+    val education: List<Education> = emptyList(),
+    val skills: List<Skill> = emptyList(),
+    val languages: List<Language> = emptyList(),
+    val summary: String = ""
 )
