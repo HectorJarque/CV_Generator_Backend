bcf87cf0f445665b279b8311770218ffc3ec4288
Kallik <h.jarqueg@gmail.com>
2026-05-20 12:09:34 +0200

    [feature/create-models]: create language model and enum

--- src/main/kotlin/org/example/cvgenerator/domain/model/Language.kt
+++ src/main/kotlin/org/example/cvgenerator/domain/model/Language.kt
@@ -1,5 +1,15 @@
 package org.example.cvgenerator.domain.model
 
 data class Language(
-    val id: Int,
+    val name: String = "",
+    val level: LanguageLevel = LanguageLevel.B1
 )
+enum class LanguageLevel {
+    A1,
+    A2,
+    B1,
+    B2,
+    C1,
+    C2,
+    NATIVE
+}
